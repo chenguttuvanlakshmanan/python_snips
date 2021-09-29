dfae88a107173d24aa02c35eabe250fecdefea3d
chenguttuvanlakshmanan <lchenmbss@gmail.com>
2021-09-29 16:37:14 +0300

    third commit

--- Testing/test_main.py
+++ Testing/test_main.py
@@ -1,6 +1,8 @@
 import main
 import pytest
 
+#******************************Pytest notes*****************************************************************************
+
 # the test file should start with test or end with test 
 # to run the test type pytest in the terminal
 # stop running functions after first fail pytest -x  pytest --maxfial=2
@@ -8,14 +10,20 @@ import pytest
 # to run test in a class pytest -k TestClass
 # to run particular test function inside a file pytest test_mod.py::test_func
 
+# Will run all tests which are decorated with the @pytest.mark.slow decorator => ( pytest -m slow)
+# to attach debugger on failure we can use pytest --pdb
 
-#*****************testing report
+
+# ==>>>> Detailed Report
+
 # to get detailed report summary of the test performed pytest -ra,-ra [ all expect pass] , -rp [ all passed test]
 # -rs[skipped tests]
 
-# simple function test using pytest
+#******************************Pytest notes*****************************************************************************
+
+# simple function test using pytest, we can fail the test by changing input or result
 def test_add():
-    assert(main.add(5,4) == 10)
+    assert(main.add(5,4) == 9)
 
 # this will group tests
 class TestClass:
